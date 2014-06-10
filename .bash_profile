562148ba8b00177fdec2430844dce20b72f72634
d.kirichenko <d.kirichenko@fun-box.ru>
2014-06-10 10:46:12 +0400

    удалил остатки bash, добавил новый конфиг вима без janus

--- .bash_profile
+++
@@ -1,7 +0,0 @@
-alias g='git'
-alias s='spring'
-alias sr='spring rspec'
-alias trake='RAILS_ENV=test rake'
-alias b='bundle exec'
-
-eval "$(rbenv init -)"
--- create_links.sh
+++ create_links.sh
@@ -1,6 +1,4 @@
-ln .bashrc ~/.bashrc
-ln .bash_profile ~/.bash_profile
 ln .gitconfig ~/.gitconfig
 ln .gitignore_global ~/.gitignore_global
-ln .vimrc.before ~/.vimrc.before
 ln .zshrc ~/.zshrc
+ln .vimrc ~/.vimrc
